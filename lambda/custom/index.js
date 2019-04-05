162d77007c73a0693ffa546073144d73879824a2
Swapnil Agrawal <swapnil@MacBook-Pro-2.local>
2019-04-05 00:32:52 -0500

    Initialization

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,35 +2,103 @@
 /* eslint-disable  no-console */
 
 const Alexa = require('ask-sdk-core');
+var {google} = require('googleapis');
+var fs = require('fs');
+var googleAuth = require('google-auth-library');
+var client;
+
+function searchListByKeyword(auth, requestData) {
+  var service = google.youtube('v3');
+  var parameters = requestData['params'];
+  parameters['auth'] = auth;
+  service.search.list(parameters, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    console.log(response);
+  });
+}
+
+fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+  if (err) {
+    console.log('Error loading client secret file: ' + err);
+    return;
+  }
+
+  client = JSON.parse(content);
+
+});
+
 
 const LaunchRequestHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
   },
-  handle(handlerInput) {
-    const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
-
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
-  },
+  handle(handlerInput) { 
+    console.log('session details', handlerInput.requestEnvelope.session.sessionId);
+    console.log('accessToken: ', handlerInput.requestEnvelope.context.System.user.accessToken);
+    var accessToken = handlerInput.requestEnvelope.context.System.user.accessToken;
+    if (accessToken == undefined) {
+      //no token, show link account card
+      handlerInput.responseBuilder.shouldEndSession(true).say('Your Youtube account is not linked');
+      return true;
+    } else {
+      // i've got a token! move forward
+      const speechText = 'What do you want me to play on Youtube?';
+      return handlerInput.responseBuilder
+        .speak(speechText)
+        .reprompt(speechText)
+        .withSimpleCard(speechText)
+        .getResponse();
+    }
+  }
 };
 
-const HelloWorldIntentHandler = {
+const StreamYoutubeIntentHandler = {
   canHandle(handlerInput) {
     return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+      && handlerInput.requestEnvelope.request.intent.name === 'StreamYoutubeIntent';
   },
   handle(handlerInput) {
-    const speechText = 'Hello World!';
+    
+    var keyword = handlerInput.requestEnvelope.request.intent.slots.value.value;
+    
+    const speechText = keyword;
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
-  },
+    // return handlerInput.responseBuilder
+    //   .speak(speechText)
+    //   .withSimpleCard('Hello World', speechText)
+    //   .getResponse();
+    
+      var requestData = {
+                          'params': {
+                            'maxResults': '10',
+                            'part': 'snippet',
+                            'q': keyword,
+                            'type': 'video'}
+                        }
+
+      var credentials = client;
+      var clientSecret = credentials.web.client_secret;
+      var clientId = credentials.web.client_id;
+      var redirectUrl = credentials.web.token_uri;
+      var auth = new googleAuth();
+      var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+      oauth2Client.credentials = JSON.parse(handlerInput.requestEnvelope.context.System.user.accessToken);
+
+      var res = searchListByKeyword(auth, requestData);
+      if(res != undefined){
+        return handlerInput.responseBuilder
+        .speak("Obtained")
+        .withSimpleCard('Hello World', speechText)
+        .getResponse();
+      }
+      
+      console.log(res);
+
+      
+  }
 };
 
 const HelpIntentHandler = {
@@ -95,7 +163,7 @@ const skillBuilder = Alexa.SkillBuilders.custom();
 exports.handler = skillBuilder
   .addRequestHandlers(
     LaunchRequestHandler,
-    HelloWorldIntentHandler,
+    StreamYoutubeIntentHandler,
     HelpIntentHandler,
     CancelAndStopIntentHandler,
     SessionEndedRequestHandler
